3daa4e1b3eec14500b3340cc948ab5322df8e71e
faney <laofa_iphone@163.com>
2019-03-21 21:51:55 +0800

    [Feature] 修改图片 Type: 新功能 Project: SystemFont

--- appFont/appFont/main.m
+++ appFont/appFont/main.m
@@ -2,8 +2,8 @@
 //  main.m
 //  appFont
 //
-//  Created by 张永强 on 16/11/25.
-//  Copyright © 2016年 LDWL. All rights reserved.
+//  Created by Yuanhai on 15/3/19.
+//  Copyright © 2019年 Yuanhai. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
